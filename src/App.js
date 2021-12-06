e4c355257b8bad3e74cf7a07d6003537e59475cd
Bala Vallivel <vvbala1995@gmail.com>
2021-12-06 23:02:07 +0530

    basic component and its routes are created. commiting this for future refrence.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Profile from './Bala/Profile';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" style={{width: 300 + 'px'}} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +24,13 @@ function App() {
         >
           Learn React
         </a>
+        <Link to="/" className="App-link person-link">INDEX</Link>
+        <Link to="/bala" className="App-link person-link">BALA</Link>
+
+        <Routes>
+            <Route path="bala" element={<Profile />} />
+        </Routes>
+
       </header>
     </div>
   );
---
+++ src/Bala/Profile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+class Profile extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Page BALA</h1>
+                <p>Sample component</p>
+            </div>
+        );
+    }
+}
+
+export default Profile;
